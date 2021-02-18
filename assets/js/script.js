f15942d675589590d9e80005494cd240461038a0
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-02-18 15:24:42 -0800

    capture form field type values

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,22 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    // create new task item
+    // create new list item
     var listItemEl = document.createElement("li");
-    // style new task item
     listItemEl.className = "task-item";
-    // add text
-    listItemEl.textContent = "This is a new task.";
-    // append element to task list
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entre list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
